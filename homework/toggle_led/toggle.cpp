81cb81bf50821282091a3c266f1fb89462232c26
Gurvinder Singh <51168245+bhu11argur1@users.noreply.github.com>
2020-02-24 19:37:52 -0500

    Update toggle.cpp

--- homework/toggle_led/toggle.cpp
+++ homework/toggle_led/toggle.cpp
@@ -3,7 +3,7 @@
 #include<string>
 using namespace std;
 
-#define LED3_PATH "/sys/class/gpio/gpio200"
+#define LED3_PATH "/sys/class/gpio/gpio20"
 
 void writeLED(string filename, string value){
    fstream fs;
